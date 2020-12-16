0245fe27fb16269cb6f73977287d6a1641a3184e
xeno <xeno.freenode@gmail.com>
2020-12-16 07:39:25 +0100

    refactored

--- day16/BaseTypes.fs
+++ day16/BaseTypes.fs
@@ -3,6 +3,7 @@ module day16.BaseTypes
 open System
 open day16.Input
 
+type Ticket = uint64[]
 type Range (startAt:uint64,endAt:uint64) as self =
     override this.ToString() = sprintf "%d-%d" startAt endAt
     member this.contains (value:uint64) : bool = value >= startAt && value <= endAt 
@@ -14,5 +15,13 @@ type Field (name: String, range1:Range,range2:Range) as self =
         let range2 = Range(fst f.Range2,snd f.Range2)
         Field(f.Name,range1,range2)
     member this.isValid (value:uint64) : bool =
-        range1.contains value || range2.contains value  
-
+        range1.contains value || range2.contains value
+        
+type TicketData (fields:Field[], yours:Ticket, nearby:Ticket[]) as self =
+    override this.ToString () = sprintf "TicketData fields:%A yours:%A nearby:%A" fields yours nearby
+    member this.Fields = fields
+    member this.Yours = yours
+    member this.Nearby = nearby 
+    member this.replaceNearby (newNearby:Ticket[]) =
+        TicketData(fields,yours,newNearby)
+   
--- day16/Program.fs
+++ day16/Program.fs
@@ -2,31 +2,39 @@
 
 open day16.BaseTypes
 open day16.Input
-
-        
-                
-let completelyInvalidValues (fields:Field[]) (ticket:uint64[]) : bool =
+                 
+let completelyInvalid (fields:Field[]) (ticket:Ticket) : bool =
     let invalid (value:uint64) : bool =
         fields |> Seq.filter (fun (field:Field) -> field.isValid value) |> Seq.isEmpty
     ticket |> Seq.filter (invalid) |> Seq.isEmpty
 
-let completelyInvalid (fields:Field[]) (ticket:uint64[]) : uint64[] =
+let completelyInvalidValues (fields:Field[]) (ticket:Ticket) : uint64[] =
     let invalid (value:uint64) : bool =
         fields |> Seq.filter (fun (field:Field) -> field.isValid value) |> Seq.isEmpty
     ticket |> Seq.filter (invalid) |> Seq.toArray 
 
 let task1 fields nearby = 
-    let invalidFields = nearby |> Seq.map (completelyInvalid fields) |> Seq.concat
+    let invalidFields = nearby |> Seq.map (completelyInvalidValues fields) |> Seq.concat
     let answer1 = invalidFields |> Seq.sum 
     printfn "invalidFields = %A" invalidFields
     printfn "Answer 1 = %d" answer1 
 
+let withoutCompletelyInvalid (ticketData:TicketData) :TicketData =
+    let validNearby = ticketData.Nearby |> Seq.filter (completelyInvalid ticketData.Fields) |> Seq.toArray 
+    ticketData.replaceNearby validNearby     
+
+let task2 (ticketData:TicketData) =
+    let data1 = withoutCompletelyInvalid ticketData  
+    printf "$$$ %A" data1 
+
 [<EntryPoint>]
 let main argv =
-    let input = "/Users/xeno/projects/aoc2020/day16_fs/input.txt"
+    let input = "/Users/xeno/projects/aoc2020/day16_fs/input2.txt"
     let inputData = readSplitInput input    
     let fields = inputData.Fields |> Seq.map (Field) |> Seq.toArray 
     let nearby = inputData.Nearby
-    task1 fields nearby 
+    let data:TicketData = TicketData(fields,inputData.Yours,inputData.Nearby)
+    task1 fields nearby
+    task2 data 
     
     0 // return an integer exit code
